2001960e9e473c30b57d782e9b33ec36dbd2700c
MounikaAlloju <mounika.alloju@gmail.com>
2022-02-24 14:59:43 +0530

    UC5 added

--- src/com/bridgelabz/EmployeeWageForMultipleCompanies.java
+++ src/com/bridgelabz/EmployeeWageForMultipleCompanies.java
@@ -13,25 +13,28 @@ public class EmployeeWageForMultipleCompanies {
 		final int parttime=1;
 		int empHrs=0;
 		int empWage=0;
+		int monthlyempWage=0;
 		int empCheck=0;
+		for(int day=1;day<=20;day++) {
+			empCheck=(int)Math.floor(Math.random()*10) % 3 ;
+			//System.out.println(empCheck);
+			switch (empCheck){    
+			case 2:    
+				empHrs=8;
+				break;  //optional  
+			case 1:    
+				empHrs=4;
+				break; 
+			default:     
+				empHrs=0;  
+				break;
+			} 
 
-		empCheck=(int)Math.floor(Math.random()*10) % 3 ;
-		//System.out.println(empCheck);
-		switch (empCheck){    
-		case 2:    
-			empHrs=8;
-			break;  //optional  
-		case 1:    
-			empHrs=4;
-			break; 
-		default:     
-			empHrs=0;  
-			break;
-		} 
-
-		empWage= EMP_RATE_PER_HOUR * empHrs;
-
+			empWage= EMP_RATE_PER_HOUR * empHrs;
+			monthlyempWage=monthlyempWage+empWage;
+		}
 		System.out.println("Emp Wage is "+empWage);
+		System.out.println("Monthly Employee Wage is "+monthlyempWage);
 
 	}
 
