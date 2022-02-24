a54ea460c4b52eabe46a8569b316ec95e3be82ec
MounikaAlloju <mounika.alloju@gmail.com>
2022-02-24 15:09:35 +0530

    UC7 added

--- src/com/bridgelabz/EmployeeWageForMultipleCompanies.java
+++ src/com/bridgelabz/EmployeeWageForMultipleCompanies.java
@@ -1,45 +1,61 @@
 package com.bridgelabz;
 
-public class EmployeeWageForMultipleCompanies {
+public class EmployeeWageForMultipleCompanies{
 
-	//constants
-	public static final int EMP_RATE_PER_HOUR=20;
-	//method to calculate daily employee wage of the employee
-	private static void calculateDailyWage() {
+	//declaring constants
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	/*
+	 * public static final int EMP_RATE_PER_HOUR=20; public static final int
+	 * NUM_OF_WORKING_DAYS=20; public static final int MAX_HRS_IN_MONTH=10;
+	 */
 
+	private final String company;
+	private final int empRatePerour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
 
-		//variables
-		final int fulltime=2;
-		final int parttime=1;
-		int empHrs=0;
-		int empWage=0;
-		int monthlyempWage=0;
-		int empCheck=0;
-		for(int day=1;day<=20;day++) {
-			empCheck=(int)Math.floor(Math.random()*10) % 3 ;
-			//System.out.println(empCheck);
-			switch (empCheck){    
-			case 2:    
-				empHrs=8;
-				break;  //optional  
-			case 1:    
-				empHrs=4;
-				break; 
-			default:     
-				empHrs=0;  
-				break;
-			} 
-
-			empWage= EMP_RATE_PER_HOUR * empHrs;
-			monthlyempWage=monthlyempWage+empWage;
-		}
-		System.out.println("Emp Wage is "+empWage);
-		System.out.println("Monthly Employee Wage is "+monthlyempWage);
-
+	//created parametrized constructor
+	public EmployeeWageForMultipleCompanies(String company, int empRatePerour, int numOfWorkingDays, int maxHoursPerMonth) {
+		super();
+		this.company = company;
+		this.empRatePerour = empRatePerour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
 	}
+
 
 	public static void main(String[] args) {
+		EmployeeWageForMultipleCompanies dmart=new EmployeeWageForMultipleCompanies("Dmart",20,2,10);
 
-		calculateDailyWage();
+		//declaring variables
+		int empHours=0,totalEmpHours=0,totalWorkingDays=0;
+		while(totalEmpHours <= dmart.maxHoursPerMonth && totalWorkingDays <= dmart.numOfWorkingDays ) {
+
+			totalWorkingDays++;
+			int empCheck=(int)Math.floor(Math.random()*10) % 3;
+
+
+			switch(empCheck){    
+			case  IS_FULL_TIME:    
+				empHours=8;
+				break;  //optional  
+			case IS_PART_TIME:    
+				empHours=4;
+				break; 
+			default:     
+				empHours=0;  
+				break;
+			}
+			totalEmpHours+=empHours;
+			System.out.println("Day#:" +totalWorkingDays+"Emp Hr :"+empHours);
+		}
+		int totalEmpWage=dmart.empRatePerour*totalEmpHours;
+		System.out.println("Total Employee Wage for Company " +dmart.company+  "  is  " +totalEmpWage);
+
 	}
 }
+
+
+
+
